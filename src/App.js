b81f082f4fd9190e5c95fa0a8d076ad33da6088c
faisal binali <you@example.com>
2022-05-08 10:33:39 -0700

    d

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+import products from "./products";
+function Person(props) {
+  return (
+    <>
+      <h1>{props.name} </h1>
+      <h1>{props.price} </h1>
+    </>
+  );
+}
 
 function App() {
-  return (
-    <div>
-      <h1>React App </h1>
-      <p> description</p>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-        alt="logo"
-      />
-    </div>
-  );
+  const people = products.map((item) => (
+    <Person name={item.name} price={item.price} />
+  ));
+  return <div>{people}</div>;
 }
 
 export default App;
---
+++ src/products.js
@@ -0,0 +1,18 @@
+let products = [
+  {
+    name: "aaa",
+    price: 212,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    name: "bbb",
+    price: 222,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    name: "ccc",
+    price: 232,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+];
+export default products;
