578520d84273ebfb5d2dead26a46c239b9e13123
faisal binali <you@example.com>
2022-05-09 07:35:40 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import products from "./products";
+import Home from "./comp/Home";
 function Person(props) {
   return (
     <>
       <h1>{props.name} </h1>
-      <h1>{props.price} </h1>
+      <h4>{props.price} </h4>
+      <img src={props.Img} />
     </>
   );
 }
 
 function App() {
   const people = products.map((item) => (
-    <Person name={item.name} price={item.price} />
+    <Person name={item.name} price={item.price} Img={item.Img} />
   ));
   return <div>{people}</div>;
 }
---
+++ src/comp/Home.js
@@ -0,0 +1,18 @@
+let Home = [
+  {
+    name: "Item 1",
+    price: 212,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    name: "Item 2",
+    price: 222,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    name: "Item 3",
+    price: 232,
+    Img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+];
+export default Home;
